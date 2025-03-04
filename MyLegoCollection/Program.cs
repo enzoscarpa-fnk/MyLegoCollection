7240061ebfd682a6af329e90f425f066bebe4854
enzoscarpa-fnk <159703846+enzoscarpa-fnk@users.noreply.github.com>
2025-03-04 17:59:42 +0100

    Added deletion of collection with confirmation process

--- MyLegoCollection/Program.cs
+++ MyLegoCollection/Program.cs
@@ -2,6 +2,7 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyLegoCollection;
+using MyLegoCollection.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
